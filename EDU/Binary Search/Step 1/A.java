dac10a3753de2fff5e8846dd3e2584ed5fcb6116
Reynell Quevedo <reynell4587@hotmail.com>
2022-11-11 21:15:09 -0500

    Binary Search - Solved 1A

---
+++ EDU/Binary Search/Step 1/A.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.*;
+
+public class A {
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        int n, k;
+        st = new StringTokenizer(in.readLine());
+        n = Integer.valueOf(st.nextToken());
+        k = Integer.valueOf(st.nextToken());
+        int[] a = new int[n];
+        st = new StringTokenizer(in.readLine());
+        for(int i = 0 ; i < n ; ++i){
+            a[i] = Integer.valueOf(st.nextToken());
+        }
+        Arrays.sort(a);
+        st = new StringTokenizer(in.readLine());
+        for(int i = 0 ; i < k ; ++i){
+            int x = Integer.valueOf(st.nextToken());
+            out.write( (a[bs(a,x)] == x) ? "YES\n" : "NO\n");
+        }
+        in.close();
+        out.flush();
+        out.close();
+    }
+
+    private static int bs(int[] a, int x) {
+        int l = 0, r = a.length - 1, mid ;
+        while(l < r){
+            mid = (l + r + 1)/2;
+            if(a[mid] <= x){
+                l = mid;
+            }else{
+                r = mid - 1;
+            }
+        }
+        return l;
+    }
+}
